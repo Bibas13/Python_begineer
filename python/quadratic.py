f3d2b844e6888abb6337f892726b730578710423
Bibas13 <bibas3630@gmail.com>
2025-01-11 13:22:58 +0545

    first change

--- python/quadratic.py
+++ python/quadratic.py
@@ -3,15 +3,15 @@ import math
 name = input("What is your name: ").upper()
 print(name,"Give the value for the solution, ")
 
-a=float(input('enter the coefficient of x^2: '))
-b=float(input('enter the coefficient of x: '))
-c=float(input('enter the coefficient of c: '))
+a=float(input('enter the coefficient of x^2, a= '))
+b=float(input('enter the coefficient of x, b = '))
+c=float(input('enter the value of c = '))
 
-gh = b*b - 4*a*c
+gh = b**2 - 4*a*c
 
 while True:
     
-    print('the rooot of given qudratic euation is :').upper()
+    print('the rooot of given qudratic euation is :')
 
     if a >0 and gh >0 :
         #for normal quadratic equation
@@ -38,10 +38,10 @@ while True:
         break
     
     if gh < 0:
+        print("\n The given equation has imaginary solution")
         real_part = -b/(2*a)
-        imaginary_part = math.sqrt(abs(gh))/(2*a)
+        imaginary_part = math.sqrt(abs(gh))/(2*a) 
         print("Real part is : ",real_part,"\n Imaginary root is :",imaginary_part)
-        # print the value with tuple process:
         print(f"first root is: {real_part} + {imaginary_part}i \n Second root is: {real_part}- {imaginary_part}i")
         #print the value with simple process :
         #print("first root is: ",real_part,"+", imaginary_part,"i","\n Second root is: ",real_part,"-",imaginary_part,"i")
